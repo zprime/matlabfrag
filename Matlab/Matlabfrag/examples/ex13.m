635b19b2d193c375e04ea062608514a9176db9e3
zebb <zebb@59ff2259-216f-418c-b63f-632691616972>
2009-10-14 03:12:04 +0000

    v0.6.10 matlabfrag documentation. - Removed section about plotyy using manual ticks. - Updated ex13.m to use weird tick positions while scaling.
    git-svn-id: file:///tmp/matlabfragRepo@46 59ff2259-216f-418c-b63f-632691616972

--- Matlab/Matlabfrag/examples/ex13.m
+++ Matlab/Matlabfrag/examples/ex13.m
@@ -8,8 +8,8 @@ y1 = 1e-6*rand(1,2);
 y2 = 1e-6*rand(1,2);
 x = [0 1e-3];
 hax = plotyy(x,y1,x,y2);
-set(hax(1),'ylim',[0 1e-6],'ytickmode','auto','yticklabelmode','auto');
-set(hax(2),'ylim',[0 1e-6],'ytickmode','auto','yticklabelmode','auto');
+set(hax(1),'xtick',1e-3*[0 7/32 1/2 47/64 1]);
+set(hax(2),'ylim',[0 1e-6],'ytick',1e-6*[0 1/3 2/3 1],'yticklabelmode','auto');
 set(hax(2),'xticklabel','');
 %% The following is excluded from userguide
 matlabfrag('graphics/ex13');
