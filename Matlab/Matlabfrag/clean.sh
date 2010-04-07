7db98d83f2a0dd8b3ccd9bd3fd170242d290ec0d
zebb <zebb@59ff2259-216f-418c-b63f-632691616972>
2010-04-07 10:02:18 +0000

    It's been a while. Thought I'd better commit. - Updated matlabfrag userguide. - Fixed nu vs nu^2 issue in Chapters 5 and 6. - Have LFR Hinf gain scheduling basically working.
    git-svn-id: file:///tmp/matlabfragRepo@60 59ff2259-216f-418c-b63f-632691616972

--- Matlab/Matlabfrag/clean.sh
+++ Matlab/Matlabfrag/clean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 rm matlabfrag.zip examples/userguide.log examples/userguide.toc examples/userguide.aux \
   examples/userguide.out examples/userguide.pdf
+rm examples/testing.out examples/testing.pdf examples/testing.synctex.gz examples/testing.log examples/testing.aux
 rm -fr examples/graphics
 rm examples/userguide.synctex.gz
